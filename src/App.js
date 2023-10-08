c989ae25c21c9954ed9c97a7fcb22bcdae54dee0
KoushikVijji <143944134+KoushikVijji@users.noreply.github.com>
2023-10-08 23:15:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
-function App() {
+import React, { useState } from "react";
+
+const App= () => {
+  const [result, setResult] = useState("");
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+  }
+  const clear =() => {
+    setResult("");
+  }
+  const calculate = ()=>{
+    try{
+      setResult(eval(result).toString());
+    } catch(err){
+      setResult('Error');
+    }
+    
+  }
   return (
-    <div className="container">
-      <section className="screen">
-        0
-      </section>
-      <section className="button-container">
-          <div className="column">
-            <button>7</button>
-            <button>4</button>
-            <button>1</button>
-            <button>0</button>
-          </div>
-          <div className="column">
-            <button>8</button>
-            <button>5</button>
-            <button>2</button>
-            <button>.</button>
-          </div>
-          <div className="column">
-            <button>9</button>
-            <button>6</button>
-            <button>3</button>
-            <button>=</button>
-          </div>
-          <div className="column-special">
-            <button>&larr;</button>
-            <button>&divide;</button>
-            <button>&times;</button>
-            <button>&minus;</button>
-            <button>+</button>
-          </div>
-      </section>
-    </div>
+    <>
+      <div className="container">
+        <section className="screen">
+          {result}
+        </section>
+        <section className="button-container">
+            <div className="column">
+              <button name="7" onClick={handleClick}>7</button>
+              <button name="4" onClick={handleClick}>4</button>
+              <button name="1" onClick={handleClick}>1</button>
+              <button name="0" onClick={handleClick}>0</button>
+            </div>
+            <div className="column">
+              <button name="8" onClick={handleClick}>8</button>
+              <button name="5" onClick={handleClick}>5</button>
+              <button name="2" onClick={handleClick}>2</button>
+              <button name="." onClick={handleClick}>.</button>
+            </div>
+            <div className="column">
+              <button name="9" onClick={handleClick}>9</button>
+              <button name="6" onClick={handleClick}>6</button>
+              <button name="3" onClick={handleClick}>3</button>
+              <button name="=" onClick={calculate}  id="result">=</button>
+            </div>
+            <div className="column-special">
+              <button onClick={clear} id="clear">&larr;</button>
+              <button name="/" onClick={handleClick}>&divide;</button>
+              <button name="*" onClick={handleClick}>&times;</button>
+              <button name="-" onClick={handleClick}>&minus;</button>
+              <button name="+" onClick={handleClick}>+</button>
+            </div>
+        </section>
+      </div>
+    </>
   );
 }
 
